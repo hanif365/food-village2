3116342aaa31c5c3956a670c21b43abd3ffc25b9
hanif365 <m.a.hanifkhan64@gmail.com>
2021-02-07 20:53:07 +0600

    added foreach and arrow function

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
-function menuOrder() {
+menuOrder = () => {
     const menu = document.getElementById('search-meal-field').value;
+    console.log(menu);
     mealName(menu);
     document.getElementById('search-meal-field').value = "";
+
 }
 
-function mealName(mealName) {
+mealName = (mealName) => {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
         .then(response => response.json())
         .then(data => {
+            if (data.meals == null) {
+                console.log("HIIII");
+                errorControl();
+                return;
+            }
+
+
             const showMeal = document.getElementById('display-meal');
 
-            for (let i = 0; i < data.meals.length; i++) {
-                const menuItem = data.meals[i];
+            // array.forEach(element => {
+                
+            // });
+            // const value = data.meals.length;
+            const value = data.meals;
+
+            value.forEach(menuItem => {
                 const mealItem = document.createElement('div');
                 mealItem.className = 'col';
                 const menuInfo = `
@@ -26,13 +40,32 @@ function mealName(mealName) {
                 mealItem.innerHTML = menuInfo;
                 showMeal.appendChild(mealItem);
 
-            }
+            });
+
+            // for (let i = 0; i < data.meals.length; i++) {
+            //     console.log('Meal', mealName);
+            //     const menuItem = data.meals[i];
+            //     const mealItem = document.createElement('div');
+            //     mealItem.className = 'col';
+            //     const menuInfo = `
+            //         <div id="${menuItem.idMeal}" onclick = 'MealDetails("${menuItem.idMeal}")' class="card" style="width: 18rem;">
+            //             <img src="${menuItem.strMealThumb}" class="card-img-top" alt="...">
+            //             <div class="card-body">
+            //                 <p class="card-text">${menuItem.strMeal}</p>
+            //             </div>
+            //         </div>
+            //     `;
+
+            //     mealItem.innerHTML = menuInfo;
+            //     showMeal.appendChild(mealItem);
+
+            // }
 
         })
 
 }
 
-function MealDetails(id) {
+MealDetails = (id) => {
     document.getElementById('display-details').style.display = "block";
     document.getElementById("display-meal").style.opacity = "0.5";
 
@@ -55,18 +88,48 @@ function MealDetails(id) {
             ingredient(5);
             ingredient(6);
 
-            function ingredient(id){
-                document.getElementById(`li-ingredient-${id}`).innerHTML = data['meals']['0'][`strMeasure${id}`] +' '+ data['meals']['0'][`strIngredient${id}`];
+            function ingredient(id) {
+                document.getElementById(`li-ingredient-${id}`).innerHTML = data['meals']['0'][`strMeasure${id}`] + ' ' + data['meals']['0'][`strIngredient${id}`];
             }
+
+            // ingredient = (id) => {
+            //     document.getElementById(`li-ingredient-${id}`).innerHTML = data['meals']['0'][`strMeasure${id}`] + ' ' + data['meals']['0'][`strIngredient${id}`];
+            // }
 
         })
 
 }
 
 
-function closeDisplayDetails(){
+
+closeDisplayDetails = () => {
     document.getElementById('display-details').style.display = "none";
     document.getElementById("display-meal").style.opacity = "1";
-    
 }
+
+errorControl = () => {
+    document.getElementById('error-message').style.display = "block";
+}
+
+errorClose = () => {
+    document.getElementById('error-message').style.display = "none";
+}
+
+// function closeDisplayDetails() {
+//     document.getElementById('display-details').style.display = "none";
+//     document.getElementById("display-meal").style.opacity = "1";
+
+// }
+
+// function errorControl() {
+//     document.getElementById('error-message').style.display = "block";
+// }
+
+
+
+// function errorClose() {
+//     document.getElementById('error-message').style.display = "none";
+// }
+
+
 
